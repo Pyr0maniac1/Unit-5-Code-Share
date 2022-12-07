c8c0f4c7ca55a1820fcad1a7ee9589038445fd92
Max <58262387+Pyr0maniac1@users.noreply.github.com>
2022-12-07 15:09:09 -0500

    full script

--- Script.js
+++ Script.js
@@ -1,1 +1,36 @@
-script.js 
+var toDoListInput = document.createElement('input');
+toDoListInput.type = 'text';
+toDoListInput.placeholder = 'Add a task';
+toDoListInput.addEventListener('keypress', function(e) {
+  if (e.keyCode === 13) {
+    addToDoListItem(this.value);
+    this.value = '';
+  }
+});
+document.body.insertBefore(toDoListInput, toDoList);
+
+
+var toDoList = document.createElement('ol');
+document.body.appendChild(toDoList);
+var toDoListItems = [];
+var toDoListItemCount = 0;
+var toDoListItem = function(text) {
+  this.text = text;
+  this.id = toDoListItemCount++;
+  this.element = document.createElement('li');
+  this.element.innerHTML = this.text;
+  this.element.id = this.id;
+  this.element.addEventListener('click', function() {
+    toDoList.removeChild(this);
+    toDoListItems.splice(this.id, 1);
+    for (var i = this.id; i < toDoListItems.length; i++) {
+      toDoListItems[i].id--;
+      toDoListItems[i].element.id--;
+    }
+    toDoListItemCount--;
+  });
+  toDoList.appendChild(this.element);
+};
+var addToDoListItem = function(text) {
+  toDoListItems.push(new toDoListItem(text));
+};
